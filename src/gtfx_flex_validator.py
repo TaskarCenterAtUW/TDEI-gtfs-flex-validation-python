08e4a37993bc5b6f6ea5dd1e7776bbccd5f78f41
Sujata <sujatam@gaussiansolutions.com>
2023-01-23 21:02:44 +0530

    - Fixed multiple things  - Updated validator logic  - Added publisher  - Added a new API to publish a message  - Refactor code

--- src/gtfx_flex_validator.py
+++ src/gtfx_flex_validator.py
@@ -1,4 +1,5 @@
 import uuid
+import urllib.parse
 from python_ms_core import Core
 from python_ms_core.core.queue.models.queue_message import QueueMessage
 from .config import Settings
@@ -12,26 +13,42 @@ class GTFSFlexValidator:
     def __init__(self):
         Core.initialize()
         settings = Settings()
-        self.listening_topic = Core.get_topic(topic_name=settings.subscription_topic_name, callback=self.process)
+        self._subscription_name = settings.subscription_name
+        self.listening_topic = Core.get_topic(topic_name=settings.subscription_topic_name)
         self.publish_topic = Core.get_topic(topic_name=settings.publishing_topic_name)
         self.logger = Core.get_logger()
-        self.listening_topic.subscribe(subscription=settings.subscription_name)
+        self.subscribe()
+        self.storage_client = Core.get_storage_client()
 
-    def process(self, instance):
-        message = instance.get_messages()
-        if message is not None:
-            gtfs_upload_message = QueueMessage.to_dict(message)
-            upload_message = GTFSFlexUpload.data_from(gtfs_upload_message)
-            file_upload_path = upload_message.data.file_upload_path
-            file_relative_path = file_upload_path.split('/')[-1]
-            if file_relative_path:
-                validation = Validation(file_name=file_relative_path)
-                is_file_valid = validation.is_valid
-                validation_message = validation.validation_message
-                self.send_status(valid=is_file_valid, upload_message=upload_message,
-                                 validation_message=validation_message)
-        else:
-            print('No Message')
+    def subscribe(self):
+        def process(message):
+            if message is not None:
+                gtfs_upload_message = QueueMessage.to_dict(message)
+                upload_message = GTFSFlexUpload.data_from(gtfs_upload_message)
+                file_upload_path = urllib.parse.unquote(upload_message.data.file_upload_path)
+                if file_upload_path:
+                    validation = Validation(file_path=file_upload_path)
+                    is_file_valid = validation.is_valid
+                    if is_file_valid:
+                        # Example to get the stream of a file
+                        self.get_file(file_upload_path=file_upload_path)
+
+                    validation_message = validation.validation_message
+                    self.send_status(valid=is_file_valid, upload_message=upload_message,
+                                     validation_message=validation_message)
+            else:
+                print('No Message')
+
+        self.listening_topic.subscribe(subscription=self._subscription_name, callback=process)
+
+    def get_file(self, file_upload_path=None):
+        file_path = file_upload_path
+        if 'http' in file_upload_path:
+            file_path = '/'.join(urllib.parse.unquote(file_upload_path).split('/')[4:])
+
+        container = self.storage_client.get_file('gtfsflex', file_path)
+        # Uncomment the below line to read the stream
+        # body = container.get_stream()
 
     def send_status(self, valid: bool, upload_message: GTFSFlexUpload, validation_message: str = ''):
         upload_message.data.is_valid = valid
--- src/validation.py
+++ src/validation.py
@@ -1,14 +1,11 @@
-import urllib.parse
-
-
 class Validation:
-    def __init__(self, file_name=None):
-        self.is_valid, self.validation_message = self.is_file_validate(file_full_name=file_name)
+    def __init__(self, file_path=None):
+        self.file_relative_path = file_path.split('/')[-1]
+        self.is_valid, self.validation_message = self.is_file_validate(file_full_name=self.file_relative_path)
 
     @classmethod
     def is_file_validate(cls, file_full_name=None):
-        file_path_clean = urllib.parse.unquote(file_full_name)
-        file_name = file_path_clean.split('/')[-1]
+        file_name = file_full_name.split('/')[-1]
 
         if file_name.find('invalid') != -1:
             print('Invalid file')
